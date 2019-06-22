ae21797da6156f73504aa7b36023420218ff79bc
Syed Usman <syedusman.1@gmail.com>
2019-06-22 23:11:33 +1000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useSelector,useDispatch  } from 'react-redux';
+import { AppState } from './store/configureStore';
+import { addingCustomer, deleteCustomer, deletingCustomer } from './actions/customers';
+import { Customer } from './types/customer';
 
 const App: React.FC = () => {
+
+  const [counter, setCounter] = useState(0);
+  const customers = useSelector((state: AppState) => state.customers);
+  const dispatch = useDispatch()    
+  const addCustomer = () =>{
+    const c : Customer = {
+      id: counter.toString(),
+      firstName: "first" + counter.toString(),
+      lastName: "last",
+      dob: new Date()
+    }
+    dispatch(addingCustomer(c));
+    setCounter(counter+1);
+  }
+  const deleteCustomer = (id: string) =>{
+    dispatch(deletingCustomer(id));
+  }
+  let customerHTML = customers.map( (customer) => {
+    return (
+      <div>
+        <span>{customer.firstName + ' ' + customer.lastName}</span>
+        <button onClick={()=>deleteCustomer(customer.id)}>delete button</button>
+
+      </div>
+    )
+  })
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <div onClick={ ()=>addCustomer()}>
+         {counter} {customers.length}
+       </div>
+       <div>
+       {customerHTML}
+       </div>
       </header>
     </div>
   );
