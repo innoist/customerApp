2a9285dd37208ad5ed275f5a617af9f47512a07d
Syed Usman <syedusman.1@gmail.com>
2019-06-23 20:04:20 +1000

    date control

--- src/components/AddCustomer.tsx
+++ src/components/AddCustomer.tsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Header, Image, Modal, Divider, Form } from 'semantic-ui-react'
 import { useSelector, useDispatch } from 'react-redux';
 import { AppState } from '../store/configureStore';
 import { showHideManagecustomer } from '../actions/customers';
+import DatePicker from 'react-datepicker';
+import uuid from 'uuid/v4';
+import { string } from 'prop-types';
+import { Customer } from '../types/customer';
 
-const AddCustomer : React.FC = () =>{
+
+interface IProps{
+    customer?:Customer
+}
+interface IDate{
+    dob?:Date
+}
+
+const AddCustomer : React.FC<IProps> = (customerProp) =>{
 const showOrHide = useSelector((state: AppState) => state.manageCustomerList.showHideManageCustomer);
 const dispatch = useDispatch();
+
+const [firstName,setFirstName] = useState(customerProp.customer ? customerProp.customer.firstName : '');
+const [lastName,setLastName] = useState(customerProp.customer ? customerProp.customer.lastName : '');
+// const [dob,setDob] = useState<IDate>({dob: customerProp.customer ? customerProp.customer.dob : null});
+
+const [dob,setDob] = useState(customerProp.customer ? customerProp.customer.dob : null);
+
 
 const closeAddCustomer = () =>{
     dispatch(showHideManagecustomer(false));
@@ -14,24 +33,36 @@ const closeAddCustomer = () =>{
 const addCustomer = () => {
     closeAddCustomer();
 }
+
+const selectDOB = (date: Date) =>{
+setDob(date);
+}
+const firstNameChange = (e: React.FormEvent<HTMLInputElement>) =>{
+  setFirstName(e.currentTarget.value);
+}
+const lastNameChange = (e: React.FormEvent<HTMLInputElement>) =>{
+    setLastName(e.currentTarget.value);
+}
 return (
 
     <Modal open={showOrHide} >
-    <Modal.Header>Enter the customer details</Modal.Header>
+    <Modal.Header>Enter the customer details (All fields are mandatory)</Modal.Header>
     <Modal.Content>
       <Modal.Description>
       <Form>
     <Form.Field>
       <label>First Name</label>
-      <input placeholder='First Name' />
+      <input placeholder='First Name' onChange={firstNameChange} value={firstName}/>
     </Form.Field>
     <Form.Field>
       <label>Last Name</label>
-      <input placeholder='Last Name' />
+      <input placeholder='Last Name' onChange={lastNameChange} value={lastName}/>
     </Form.Field>
     <Form.Field>
       <label>Date of Birth</label>
-      <input placeholder='Last Name' type="date" />
+      <span>
+      <DatePicker showMonthDropdown={true} maxDate={new Date()} showYearDropdown={true} selected={dob} onChange={selectDOB} placeholderText="Click to select a date" />
+      </span>
     </Form.Field>
     </Form>
       </Modal.Description>
--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import { store } from './store/configureStore';
 import "semantic-ui-css/semantic.min.css";
+import "react-datepicker/dist/react-datepicker.css";
 ReactDOM.render(
 <Provider store={store}>
 <App />
